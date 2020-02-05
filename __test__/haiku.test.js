06d641690c1258143f5e7cf3f148b06da92a17de
Student <example@example.com>
2020-02-05 10:28:23 -0800

    fail vowel count test

--- __test__/haiku.test.js
+++ __test__/haiku.test.js
@@ -1,9 +1,18 @@
 import { Haiku } from './../src/haiku.js'; 
 
 describe ('Haiku', () => {
+    let haiku;
+    const line1 = 'Hello there';
+    const line2 = 'This is a haiku';
+    const line3 = 'Maybe, maybe not';
+    beforeEach(() => {
+        haiku = new Haiku(line1, line2, line3);
+    });
     test('should show value from form', () => {
-        let haiku = new Haiku("");
-        expect(haiku.line1).toEqual("");
+        expect(haiku.line1).toEqual("Hello there");
+    });
+    test('should detect number of vowels in input', () => {
+        haiku.countVowels(line1)
+        expect(haiku.numberOfVowels).toEqual(45);
     });
 });
-
--- src/haiku.js
+++ src/haiku.js
@@ -3,8 +3,21 @@ export class Haiku {
         this.line1 = line1;
         this.line2 = line2;
         this.line3 = line3;
+        this.characters = [];
+        this.numberOfVowels = 0;
     }
-  
-} 
+    
+    countVowels(line){
+        this.characters = line.split("");
+    
+        for(let i = 0; i < this.characters.length; i++){
+            const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+            if(vowels.includes(this.characters[i])){
+                this.numberOfVowels++;
+            }
+        }
+        // return this.numberOfVowels;
+};
+}
 
 
